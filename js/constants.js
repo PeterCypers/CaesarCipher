7ee085be73efa6035afa65df93536ca90f83e14e
PeterCypers <PeterCypers@users.noreply.github.com>
2025-11-03 11:26:21 +0000

    deploy: 8e522405e77d049e5c06856e3e0110e89fee2d81

--- js/constants.js
+++ js/constants.js
@@ -6,3 +6,24 @@ export const ALPHA_LOWER = [
   'a','b','c','d','e','f','g','h','i','j','k','l','m',
   'n','o','p','q','r','s','t','u','v','w','x','y','z'
 ];
+
+const encodeToggle = document.getElementById("encodeToggle");
+const encodeContainer = document.getElementById("encodeContainer");
+const decodeContainer = document.getElementById("decodeContainer");
+const encodeForm = document.getElementById("encodeForm");
+const encodeTextArea = document.getElementById("encodeArea");
+const encRot = document.getElementById("encodeRotations");
+const encRes = document.getElementById("encodeResult");
+const encText = document.getElementById("encodedText");
+const decodeForm = document.getElementById("decodeForm");
+const decodeTextArea = document.getElementById("decodeArea");
+const decRot = document.getElementById("decodeRotations");
+const decRes = document.getElementById("decodeResult");
+const decText = document.getElementById("decodedText");
+const minusBtn = document.getElementById("minusone_btn");
+const plusBtn = document.getElementById("plusone_btn");
+
+export { encodeToggle, encodeContainer, decodeContainer, encodeForm,
+  encodeTextArea, encRot, encRes, encText, decodeForm, decodeTextArea,
+  decRot, decRes, decText, minusBtn, plusBtn
+}
